2a00905c83774da7b3ad69d591748818f6b0c3bb
Anzhela Boiko <boikoanzhela@gmail.com>
2021-10-10 22:07:03 +0300

    create privateContactsView

---
+++ src/components/PrivateContactsView/PrivateContactsView.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Contacts from 'components/Contacts';
+import Form from 'components/Form';
+import Filter from 'components/Filter';
+
+export default function PrivateContactsView() {
+  return (
+    <>
+      <Form />
+      <Filter />
+      <Contacts />
+    </>
+  );
+}
