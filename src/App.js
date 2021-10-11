49f595b457c802ce1292ae1cfeeef4931d805ebd
Anzhela Boiko <boikoanzhela@gmail.com>
2021-10-11 22:51:00 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,27 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Registration from 'components/Registration';
 import Login from 'components/Login';
 import PrivateContactsView from './components/PrivateContactsView';
+import PrivateRoute from 'components/PrivateRoute';
 import { routes } from 'utils/routes';
+import HomePage from 'components/HomePage/HomePage';
+import PublicRoute from 'components/PublicRoute/PublicRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path={routes.registration} exact component={Registration} />
-        <Route path={routes.login} exact component={Login} />
-        <Route
+        <PublicRoute path={routes.homePage} exact>
+          <HomePage />
+        </PublicRoute>
+        <Route path={routes.registration} component={Registration} />
+        <Route path={routes.login} component={Login} />
+        <PrivateRoute
           path={routes.privateContacts}
           exact
-          component={PrivateContactsView}
-        />
+          redirectTo={routes.registration}
+        >
+          <PrivateContactsView />
+        </PrivateRoute>
       </Switch>
     </BrowserRouter>
   );
