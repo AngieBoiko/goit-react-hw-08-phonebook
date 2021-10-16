6df6a48db41bcde4d1e1367c1acc54a38aed66d8
Anzhela Boiko <boikoanzhela@gmail.com>
2021-10-16 22:10:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import Registration from 'components/Registration';
-import Login from 'components/Login';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import LogIn from 'components/Login';
 import PrivateContactsView from './components/PrivateContactsView';
 import PrivateRoute from 'components/PrivateRoute';
-import { routes } from 'utils/routes';
-import HomePage from 'components/HomePage/HomePage';
+import Registration from 'components/Registration';
+import routes from 'utils/routes';
+import HomePage from 'components/HomePage';
+import AppBar from 'components/AppBar';
 import PublicRoute from 'components/PublicRoute/PublicRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <AppBar />
       <Switch>
-        <PublicRoute path={routes.homePage} exact>
+        {/* <PublicRoute path={routes.homePage} exact>
           <HomePage />
-        </PublicRoute>
-        <Route path={routes.registration} component={Registration} />
-        <Route path={routes.login} component={Login} />
-        <PrivateRoute
+        </PublicRoute> */}
+        <Route exact path={routes.registration} component={Registration} />
+        {/* <PublicRoute path={routes.login} redirectTo={routes.privateContacts}>
+          <Login />
+        </PublicRoute> */}
+        <Route exact path={routes.homePage} component={HomePage} />
+        <Route exact path={routes.login} component={LogIn} />
+        {/* <PrivateRoute
           path={routes.privateContacts}
           exact
           redirectTo={routes.registration}
         >
           <PrivateContactsView />
-        </PrivateRoute>
+        </PrivateRoute> */}
       </Switch>
     </BrowserRouter>
   );
