67f0eb0361ed7c5ec5d5d3db75def151095e8b3f
Anzhela Boiko <boikoanzhela@gmail.com>
2021-10-16 22:03:58 +0300

    Update HomePage.jsx

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function HomePage() {
-  return <h1>Hello!Welcome to phonebook App!</h1>;
+  return (
+    <>
+      <h1>Hello!Welcome to phonebook App!</h1>
+    </>
+  );
 }
